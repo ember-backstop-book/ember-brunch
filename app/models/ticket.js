f3d1486e2df5a35e60f1eac64cfdcc14e1eb24b3
Wesley Workman <wworkman@batterii.com>
2013-02-24 15:53:52 -0500

    Section: "Building a relationship between users and tickets"

--- app/models/ticket.js
+++ app/models/ticket.js
@@ -2,10 +2,12 @@ App.Ticket = DS.Model.extend({
   title: DS.attr('string'),
   description: DS.attr('string'),
   status: DS.attr('string'), // New | Open | Resolved
-  createdAt: DS.attr('date')
+  createdAt: DS.attr('date'),
+  reportedBy: DS.belongsTo('App.User'),
+  assignedTo: DS.belongsTo('App.User')
 });
 
 App.loadFixtureData(App.Ticket, [
-  { id: 1, title: 'Create a User model', description: 'Users create tickets and are assigned to tickets.', status: 'New' },
-  { id: 2, title: 'Allow ticket creation', description: 'A system should be able to create tickets', status: 'Open' }
+  { id: 1, title: 'Create a User model', description: 'Users create tickets and are assigned to tickets.', status: 'New', reportedBy: 1 },
+  { id: 2, title: 'Allow ticket creation', description: 'A system should be able to create tickets', status: 'Open', reportedBy: 2, assignedTo: 3 }
 ]);
